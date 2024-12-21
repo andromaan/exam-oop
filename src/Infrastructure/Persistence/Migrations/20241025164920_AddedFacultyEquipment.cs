10fae61155e1728bdc454bff70db4aa85e7fc668
andromaan <andrii.lavreniuk@oa.edu.ua>
2024-12-21 15:16:58 +0200

    init

---
+++ src/Infrastructure/Persistence/Migrations/20241025164920_AddedFacultyEquipment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFacultyEquipment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "equipment",
+                table: "faculties",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "equipment",
+                table: "faculties");
+        }
+    }
+}
